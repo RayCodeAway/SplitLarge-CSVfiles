de78e2985a78ce21a7ac19b75df44fd433cd0931
coderayway <78300214+coderayway@users.noreply.github.com>
2021-02-12 13:09:58 +0000

    Update CSV-FileSplitter.ps1

--- CSV-FileSplitter.ps1
+++ CSV-FileSplitter.ps1
@@ -124,19 +124,3 @@ function CSV-FileSplitter
     }
 }
 
-
-
-#(Import-Csv $FilePath | Measure-Object).Count
-$sw = new-object System.Diagnostics.Stopwatch
-$sw.Start()
-
-$FilePath = $HOME +'\Documents\Projects\ADOPT\Data8277.csv' 
-$SplitDir = $HOME +'\Documents\Projects\ADOPT\Split\' 
-
-
-CSV-FileSplitter -Path $FilePath -PartSizeBytes 35MB -SplitDir $SplitDir #-Verbose 
-
-
-$sw.Stop()
-Write-Host "Split complete in " $sw.Elapsed.TotalSeconds "seconds"
-
